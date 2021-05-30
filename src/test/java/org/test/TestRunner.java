081512d47f9ebcbcb1d708fbb3ba7851d740fd33
Sabasiva06 <sabasiva06@gmail.com>
2021-05-30 13:22:17 +0530

    technolgy work

--- src/test/java/org/test/TestRunner.java
+++ src/test/java/org/test/TestRunner.java
@@ -6,7 +6,27 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src\\test\\resources" },glue="org.stepdef")
+@CucumberOptions(features = { "src\\test\\resources" }, glue = "org.stepdef")
 public class TestRunner {
+
+	public void sampeCode() {
+		System.out.println("Sampe code");
+
+	}
+
+	public void sampleCode1() {
+		System.out.println("Sample code 1");
+
+	}
+
+	public void sampleCode2() {
+		System.out.println("Sample Code 2");
+
+	}
+
+	public void sampleCode3() {
+		System.out.println("Sample Code 3");
+
+	}
 
 }
